c7e4a8745574abef1c76ac018455e89bf4c1a01a
Aditya Bikram Arandhara <adityabikrambill@gmail.com>
2022-06-15 13:55:03 +0530

    Configured and published package

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='TemperatureConverterByAdityaBill',
+    version='0.1',
+    author="Aditya Bikram Arandhara",
+    author_email="adityabikrambill@gmail.com",
+    description="This python package is used to convert temperture values in different measurement scales.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
